de03aa9ce9f7cada43a8247b861777e0564eef67
danielpintard <100798259+danielpintard@users.noreply.github.com>
2023-10-30 17:24:47 -0400

    used clustlo for alignment, gave up on pipeline

--- SCAMP_align.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO, pairwise2
-
-file = "/Users/danielpintard/Documents/Code/Bioinformatics/scamp_align/SCAMP_coding_seqs.fasta"
-
-for record in SeqIO.parse(file, 'fasta'):
-    record 
-    
- 
